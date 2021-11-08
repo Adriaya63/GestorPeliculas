8ccd4897fc98811e193ee049126010652d78cf65
Alan <44586947+AliOlixx@users.noreply.github.com>
2021-11-08 21:55:44 +0100

    Método están conectados implementado y testeado

--- src/org/practica3/GraphHash.java
+++ src/org/practica3/GraphHash.java
@@ -2,23 +2,66 @@ package org.practica3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.gestorpeliculas.*;
 
 public class GraphHash {
 	
-	HashMap<String, ArrayList<String>> g;
+	private HashMap<String, ArrayList<String>> g;
 	public void crearGrafo(ListaActores lista) {
 		// Post: crea el grafo desde la lista de actores
-		// Los nodos son nombres de actores
+		// Los nodos son nombres de actores)
 	
 		 // COMPLETAR CÓDIGO
 		
 	}
 	
-	public boolean estanConectados(String a1, String a2) {
+	
+	public GraphHash(){
+		this.g = new HashMap<String, ArrayList<String>>();
+		
+	}
+	
+	public boolean estanConectados(String inicio, String objetivo) {
 		// COMPLETAR CÓDIGO
-		return false;
+		Queue<String> porExaminar = new LinkedList<String>();
+		HashSet<String> porExaminarSet = new HashSet<String>();
+		HashSet<String> examinados = new HashSet<String>();
+		
+		porExaminar.add(inicio); 
+		porExaminarSet.add(inicio);
+		boolean encontrado = false;
+		
+		while(!encontrado && !porExaminar.isEmpty()) {
+			String act = porExaminar.remove();
+			porExaminarSet.remove(act);
+			
+			if(act.equals(objetivo)) {
+					encontrado = true;
+					
+			}else {
+				examinados.add(act);
+				
+				for(String x: g.get(act)) {
+					//Añadimos los relacionados con act a la cola si no estan 
+					// siendo examinados o si ya han sido examinados
+					if(!examinados.contains(x) && !porExaminar.contains(x)) {
+						porExaminar.add(x);
+						porExaminarSet.add(x);
+					}
+				}
+			}
+		}
+		
+		return encontrado;
+	}
+	
+	public static void main(String args[]) {
+		GraphHash myGraph = new GraphHash();
+		myGraph.pruebaEstanConectados();
 	}
 	
 	public void print(){
@@ -32,5 +75,137 @@ public class GraphHash {
 			
 			System.out.println();
 		}
+	}
+	
+	// PROGRAMAS DE RPUEBA
+	public void pruebaEstanConectados() {
+		this.crearEjemploEstanConectados();
+		
+		if(!this.estanConectados("a", "")) {
+			System.out.println("PRUEBA 0 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 0 FALLIDA");
+		}
+		
+		if(this.estanConectados("a", "j")) {
+			System.out.println("PRUEBA 1 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 1 FALLIDA");
+		}
+		
+		if(!this.estanConectados("a", "o")) {
+			System.out.println("PRUEBA 2 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 2 FALLIDA");
+		}
+		
+		if(this.estanConectados("o", "o")) {
+			System.out.println("PRUEBA 3 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 3 FALLIDA");
+		}
+		
+		if(this.estanConectados("m", "n")) {
+			System.out.println("PRUEBA 4 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 4 FALLIDA");
+		}
+		
+		if(!this.estanConectados("a", "n")) {
+			System.out.println("PRUEBA 5 CORRECTA");
+		}else {
+			System.out.println("PRUEBA 5 FALLIDA");
+		}
+	}
+	
+	
+	private void crearEjemploEstanConectados() {
+		this.g = new HashMap<String, ArrayList<String>>();
+
+		ArrayList<String> datos = null;
+
+		// A
+		datos = new ArrayList<String>();
+		datos.add("b");
+		datos.add("c");
+		datos.add("d");
+		g.put("a", datos);
+
+		// B
+		datos = new ArrayList<String>();
+		datos.add("a");
+		datos.add("e");
+		g.put("b", datos);
+
+		// C
+		datos = new ArrayList<String>();
+		datos.add("a");
+		datos.add("e");
+		datos.add("f");
+		g.put("c", datos);
+
+		// D
+		datos = new ArrayList<String>();
+		datos.add("a");
+		datos.add("f");
+		datos.add("g");
+		g.put("d", datos);
+
+		// E
+		datos = new ArrayList<String>();
+		datos.add("b");
+		datos.add("c");
+		datos.add("h");
+		g.put("e", datos);
+
+		// F
+		datos = new ArrayList<String>();
+		datos.add("c");
+		datos.add("d");
+		datos.add("j");
+		g.put("f", datos);
+
+		// G
+		datos = new ArrayList<String>();
+		datos.add("d");
+		g.put("g", datos);
+
+		// H
+		datos = new ArrayList<String>();
+		datos.add("e");
+		datos.add("i");
+		g.put("h", datos);
+
+		// I
+		datos = new ArrayList<String>();
+		datos.add("f");
+		g.put("i", datos);
+
+		// J
+		datos = new ArrayList<String>();
+		datos.add("h");
+		g.put("j", datos);
+
+		// K
+		datos = new ArrayList<String>();
+		datos.add("m");
+		datos.add("n");
+		g.put("k", datos);
+
+		// M
+		datos = new ArrayList<String>();
+		datos.add("k");
+		datos.add("n");
+		g.put("m", datos);
+
+		// N
+		datos = new ArrayList<String>();
+		datos.add("k");
+		datos.add("m");
+		g.put("n", datos);
+
+		// O
+		datos = new ArrayList<String>();
+		g.put("o", datos);
 	}
 }
